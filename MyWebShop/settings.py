64dc78bdd6b05c75933fbb3d5bd090393173cf2e
Elena <60672676+Berna22@users.noreply.github.com>
2020-11-06 13:35:28 +0100

    Update settings.py

--- MyWebShop/settings.py
+++ MyWebShop/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1k*@@02#f35iftxkl=!i*yhdpk)25pq=7yvgi#x12s^=8z1a=%'
+#deleted
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
